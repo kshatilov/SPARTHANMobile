f2d6b81e4ed15a13bb2c99ac4a85b4aacb590db1
kshatilov <kirill@shatilov.me>
2019-05-06 13:31:24 +0800

    SPARTHANMOBILE Setting screen GUI

--- app/src/main/java/me/symlab/kirill/sparthanmobile/SettingsActivity.java
+++ app/src/main/java/me/symlab/kirill/sparthanmobile/SettingsActivity.java
@@ -2,7 +2,6 @@ package me.symlab.kirill.sparthanmobile;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,12 +10,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
-import android.widget.Switch;
 import android.widget.TextView;
 
+import org.apache.commons.collections4.MapUtils;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -25,40 +29,44 @@ import me.symlab.kirill.sparthanmobile.Utils.SettingsStore;
 public class SettingsActivity extends Activity {
 
     private static final String TAG = "Settings Activity";
+    private Button manual;
+    private Button auto;
 
-    private void initGUI() {
-        getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+    private void updateModeSelector() {
+        if (SettingsStore.getInstance().isManual()) {
+            manual.setTextColor(getResources().getColor(R.color.colorMain, getTheme()));
+            auto.setTextColor(getResources().getColor(R.color.colorInactive, getTheme()));
+        } else {
+            manual.setTextColor(getResources().getColor(R.color.colorInactive, getTheme()));
+            auto.setTextColor(getResources().getColor(R.color.colorMain, getTheme()));
+        }
+    }
 
-        // Classifiers array
+    private void initConnectivityButtons() {
+        Map<Integer, SettingsStore.Connectivity> id2connMode = new HashMap<>();
+        id2connMode.put(R.id.udp_button, SettingsStore.Connectivity.UDP);
+        id2connMode.put(R.id.tcp_button, SettingsStore.Connectivity.TCP);
+        id2connMode.put(R.id.http_button, SettingsStore.Connectivity.HTTP);
+        Map<SettingsStore.Connectivity, Integer> connMode2id = MapUtils.invertMap(id2connMode);
+        RadioGroup connectivityRG = findViewById(R.id.connectivity_radio_group);
+        connectivityRG.check(connMode2id.get(SettingsStore.getInstance().getConnectivity()));
+        connectivityRG.setOnCheckedChangeListener((radioGroup, id) -> SettingsStore.getInstance().setConnectivity(id2connMode.get(id)));
+    }
 
-        // Use cloud switch
-        Switch cloudSwitch = findViewById(R.id.cloud_switch);
-        cloudSwitch.setTrackTintList(new ColorStateList(new int[][]{
-                new int[]{-android.R.attr.state_checked},
-                new int[]{android.R.attr.state_checked},
-        }, new int[]{
-                getResources().getColor(R.color.colorInactive, getTheme()),
-                getResources().getColor(R.color.colorMain, getTheme())
-        }));
-        cloudSwitch.setChecked(SettingsStore.getInstance().useCloud());
-        cloudSwitch.setOnCheckedChangeListener((_cloudSwitch, isChecked) -> {
-            SettingsStore.getInstance().setUseCloud(isChecked);
-        });
-
-        // Number of gestures
+    private void initCloudSwitch() {
         List<Integer> numbers = IntStream.rangeClosed(SettingsStore.MIN_GESTURES, SettingsStore.MAX_GESTURES)
                 .boxed().collect(Collectors.toList());
         Spinner numGesturesSelector = findViewById(R.id.num_gestures_selector);
-        ArrayAdapter<Integer> nAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, numbers){
-            public View getView(int position, View convertView,ViewGroup parent) {
+        final ArrayAdapter<Integer> nAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, numbers) {
+            public View getView(int position, View convertView, ViewGroup parent) {
                 TextView v = (TextView) super.getView(position, convertView, parent);
-                v.setTextSize(20);
+                v.setTextSize(40);
                 v.setTextColor(getResources().getColor(R.color.colorMain, getTheme()));
                 return v;
             }
 
-            public View getDropDownView(int position, View convertView,ViewGroup parent) {
-                View v = super.getDropDownView(position, convertView,parent);
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View v = super.getDropDownView(position, convertView, parent);
                 ((TextView) v).setGravity(Gravity.CENTER);
                 return v;
             }
@@ -68,15 +76,69 @@ public class SettingsActivity extends Activity {
         numGesturesSelector.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.d(TAG, "onItemSelected: " + i + SettingsStore.MIN_GESTURES);
+                SettingsStore.getInstance().setNumGestures(i + SettingsStore.MIN_GESTURES);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
+        numGesturesSelector.setSelection(SettingsStore.getInstance().getNumGestures() - 1);
+        nAdapter.notifyDataSetChanged();
+    }
 
-        // back button
+    private void initNumGesturesSelector() {
+        List<Integer> numbers = IntStream.rangeClosed(SettingsStore.MIN_GESTURES, SettingsStore.MAX_GESTURES)
+                .boxed().collect(Collectors.toList());
+        Spinner numGesturesSelector = findViewById(R.id.num_gestures_selector);
+        final ArrayAdapter<Integer> nAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, numbers) {
+            public View getView(int position, View convertView, ViewGroup parent) {
+                TextView v = (TextView) super.getView(position, convertView, parent);
+                v.setTextSize(40);
+                v.setTextColor(getResources().getColor(R.color.colorMain, getTheme()));
+                return v;
+            }
+
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View v = super.getDropDownView(position, convertView, parent);
+                ((TextView) v).setGravity(Gravity.CENTER);
+                return v;
+            }
+
+        };
+        numGesturesSelector.setAdapter(nAdapter);
+        numGesturesSelector.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                SettingsStore.getInstance().setNumGestures(i + SettingsStore.MIN_GESTURES);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+        numGesturesSelector.setSelection(SettingsStore.getInstance().getNumGestures() - SettingsStore.MIN_GESTURES);
+        nAdapter.notifyDataSetChanged();
+    }
+
+    private void initControlModeSelectors() {
+        manual = findViewById(R.id.button_manual);
+        auto = findViewById(R.id.button_auto);
+
+        updateModeSelector();
+
+        manual.setOnClickListener(e -> {
+            SettingsStore.getInstance().setManual(true);
+            updateModeSelector();
+        });
+
+        auto.setOnClickListener(e -> {
+            SettingsStore.getInstance().setManual(false);
+            updateModeSelector();
+        });
+    }
+
+    private void initBackButton() {
         View backButton = findViewById(R.id.back_button);
         ImageView backIcon = findViewById(R.id.back_icon);
         TextView backLabel = findViewById(R.id.back_label);
@@ -89,6 +151,27 @@ public class SettingsActivity extends Activity {
         backButton.setOnClickListener(listener);
         backIcon.setOnClickListener(listener);
     }
+
+    private void initGUI() {
+        getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+
+        // Use cloud switch
+        initCloudSwitch();
+
+        // Number of gestures
+        initNumGesturesSelector();
+
+        // Control mode Selection
+        initControlModeSelectors();
+
+        // connectivity buttons
+        initConnectivityButtons();
+
+        // back button
+       initBackButton();
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
